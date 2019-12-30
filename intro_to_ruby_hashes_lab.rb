5e1e3066a2fe78a66a3b03c382ca92e30c3d8402
Eddie Hernandez <mchh.004@gmail.com>
2019-12-30 15:47:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,8 +40,7 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  if hash[key]
-  else nil end
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
