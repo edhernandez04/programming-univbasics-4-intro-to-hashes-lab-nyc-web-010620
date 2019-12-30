11c788df8fad73ec4ec984489829d29583c5fba4
Eddie Hernandez <mchh.004@gmail.com>
2019-12-30 15:39:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,10 +32,10 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  if my_hash_creator[:key] = pioneer
-    puts my_hash_creator
-  else my_hash_creator[:key] = pioneer
-  end
+  my_hash_creator = {
+    key: "value"
+  }
+  my_hash_creator
 end
 
 def read_from_hash(hash, key)
